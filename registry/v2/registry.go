4d16fa6b8c431a522a1d30303c1714f08295b1b5
John Tobe <jtobe@ball.com>
2018-02-21 10:51:12 -0500

    Better registry variable names

--- registry/v2/registry.go
+++ registry/v2/registry.go
@@ -28,36 +28,36 @@ func New(url, org, username, password string) (*v2registry, error) {
 	return nil, err
 }
 
-func (r *v2registry) Name() string {
+func (v2 *v2registry) Name() string {
 	return "V2"
 }
 
-func (r *v2registry) Ping() error {
-	_, err := r.r.Repositories()
+func (v2 *v2registry) Ping() error {
+	_, err := v2.r.Repositories()
 	return err
 }
 
-func (r *v2registry) Repositories() ([]string, error) {
-	return r.r.Repositories()
+func (v2 *v2registry) Repositories() ([]string, error) {
+	return v2.r.Repositories()
 }
 
-func (r *v2registry) Tags(repository string) ([]string, error) {
-	return r.r.Tags(repository)
+func (v2 *v2registry) Tags(repository string) ([]string, error) {
+	return v2.r.Tags(repository)
 }
 
-func (r *v2registry) Images() ([]string, error) {
-	url := r.r.URL + "/v2/_catalog"
-	r.Print("Searching %s for Seed images...\n", url)
-	repositories, err := r.r.Repositories()
+func (v2 *v2registry) Images() ([]string, error) {
+	url := v2.r.URL + "/v2/_catalog"
+	v2.Print("Searching %s for Seed images...\n", url)
+	repositories, err := v2.r.Repositories()
 
 	var images []string
 	for _, repo := range repositories {
 		if !strings.HasSuffix(repo, "-seed") {
 			continue
 		}
-		tags, err := r.Tags(repo)
+		tags, err := v2.Tags(repo)
 		if err != nil {
-			r.Print(err.Error())
+			v2.Print(err.Error())
 			continue
 		}
 		for _, tag := range tags {
@@ -68,8 +68,8 @@ func (r *v2registry) Images() ([]string, error) {
 	return images, err
 }
 
-func (r *v2registry) ImagesWithManifests() ([]objects.Image, error) {
-	imageNames, err := r.Images()
+func (v2 *v2registry) ImagesWithManifests() ([]objects.Image, error) {
+	imageNames, err := v2.Images()
 
 	if err != nil {
 		return nil, err
@@ -80,28 +80,28 @@ func (r *v2registry) ImagesWithManifests() ([]objects.Image, error) {
 	for _, imgstr := range imageNames {
 		temp := strings.Split(imgstr, ":")
 		if len(temp) != 2 {
-			r.Print("ERROR: Invalid seed name: %s. Unable to split into name/tag pair\n", imgstr)
+			v2.Print("ERROR: Invalid seed name: %s. Unable to split into name/tag pair\n", imgstr)
 			continue
 		}
-		manifest, err := r.GetImageManifest(temp[0], temp[1])
+		manifest, err := v2.GetImageManifest(temp[0], temp[1])
 		if err != nil {
 			//skip images with empty manifests
-			r.Print("ERROR: Error reading v2 manifest for %s: %s\n Skipping.\n", imgstr, err.Error())
+			v2.Print("ERROR: Error reading v2 manifest for %s: %s\n Skipping.\n", imgstr, err.Error())
 			continue
 		}
 
-		imageStruct := objects.Image{Name: imgstr, Registry: r.r.URL, Org: r.Org, Manifest: manifest}
+		imageStruct := objects.Image{Name: imgstr, Registry: v2.r.URL, Org: v2.Org, Manifest: manifest}
 		images = append(images, imageStruct)
 	}
 
 	return images, err
 }
 
-func (r *v2registry) GetImageManifest(repoName, tag string) (string, error) {
+func (v2 *v2registry) GetImageManifest(repoName, tag string) (string, error) {
 	manifest := ""
-	mv2, err := r.r.ManifestV2(repoName, tag)
+	mv2, err := v2.r.ManifestV2(repoName, tag)
 	if err == nil {
-		resp, err := r.r.DownloadLayer(repoName, mv2.Config.Digest)
+		resp, err := v2.r.DownloadLayer(repoName, mv2.Config.Digest)
 		if err == nil {
 			manifest, err = objects.GetSeedManifestFromBlob(resp)
 		}
